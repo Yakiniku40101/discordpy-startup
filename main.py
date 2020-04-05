611619cb30afb57cd85b5ac115550fb0bb08ef0c
Yakiniku40101 <63107650+Yakiniku40101@users.noreply.github.com>
2020-04-05 18:43:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,110 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+import os, re, json, random
+from janome.tokenizer import Tokenizer
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+dict_file = "chatbot-data.json"
+dic = {}
+tokenizer = Tokenizer()
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+if os.path.exists(dict_file):
+    dic = json.load(open(dict_file, "r"))
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+def register_dic(words):
+    global dic
+    if len(words) == 0: return
+    tmp = ["@"]
+    for i in words:
+        word = i.surface
+        if word == "" or word == "\r\n" or word == "\n": continue
+        tmp.append(word)
+        if len(tmp) < 3: continue
+        if len(tmp) > 3: tmp = tmp[1:]
+        set_word3(dic, tmp)
+        if word == "。" or word == "?":
+            tmp = ["@"]
+            continue
+        # 辞書更新毎にファイル保存
+        f = open(dict_file, "w", encoding="utf-8")
+        json.dump(dic, f)
 
 
-bot.run(token)
+def set_word3(dic, s3):
+    w1, w2, w3 = s3
+    if not w1 in dic: dic[w1] = {}
+    if not w2 in dic[w1]: dic[w1][w2] = {}
+    if not w3 in dic[w1][w2]: dic[w1][w2][w3] = 0
+    dic[w1][w2][w3] += 1
+
+
+def make_sentence(head):
+    if not head in dic: return ""
+    ret = []
+    if head != "@": ret.append(head)
+    top = dic[head]
+    w1 = word_choice(top)
+    w2 = word_choice(top[w1])
+    ret.append(w1)
+    ret.append(w2)
+    while True:
+        if w1 in dic and w2 in dic[w1]:
+            w3 = word_choice(dic[w1][w2])
+        else:
+            w3 = ""
+        ret.append(w3)
+        if w3 == "。" or w3 == "？" or w3 == "": break
+        w1, w2 = w2, w3
+    return "".join(ret)
+
+
+def word_choice(sel):
+    keys = sel.keys()
+    return random.choice(list(keys))
+
+
+# botに返答させる
+def make_reply(text):
+    # まず単語を学習する
+    if text[-1] != "。": text += "。"
+    words = tokenizer.tokenize(text)
+    register_dic(words)
+    # 辞書に単語があれば、そこから話す
+    for w in words:
+        face = w.surface
+        ps = w.part_of_speech.split(',')[0]
+        if ps == "感動詞":
+            return face + "。"
+        if ps == "名詞" or ps == "形容詞":
+            if face in dic: return make_sentence(face)
+    return make_sentence("@")
+
+
+# ここからメッセージ取得&返信
+
+#
+#
+# 以下、discord処理
+#
+#
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.attachments:
+        pass
+
+    elif client.user != message.author:
+        text = message.content
+        res = make_reply(text)
+        if message.channel.id == 695559964231729153:
+            await message.channel.send(res)
